cc051ce9f2756ad29c6358ccf28185ccc76d57b8
Vipul Kannaujiya <vipulkannaujiya958@gmail.com>
2026-07-10 19:18:35 +0530

    Check Your Result

--- PassFailExamSystem.java
+++ PassFailExamSystem.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class PassFailExamSystem {
     public static void main(String[] args) {
-        
+        System.out.println("=============================================================");
+        System.out.println("                      Check Your Result                      ");
+        System.out.println("=============================================================");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Your marks : ");
+        int marks = input.nextInt();
+        if (marks < 0) {
+            System.out.println("Enter valid marks..");
+            marks = input.nextInt();
+        }
+        if (marks >= 33 && marks <= 100) {
+            System.out.println("PARTY!🎉, TUM PASS HO GAYE.....");
+        }
+        else{
+            System.out.println("AUR REELS DEKHO, FAIL HO GAYE NA....");
+        }
     }
 }
